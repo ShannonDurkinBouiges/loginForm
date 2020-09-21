4d44b641a3b3978463420a0008c583367fb2dbb3
Shannon Durkin-Bouiges <ShannonDurkinBouiges@gmail.com>
2020-09-21 15:52:25 +0200

    Added header and footer pages, and added nav bar and logo

---
+++ inc/_header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="description" oontent="A Login/Sign Up Form Example">
+    <meta name=viewport content="width=device-width, initial-scale=1">
+    <title>Sign Up Form Template</title>
+    <link rel="stylesheet" type="text/css" href="">
+</head>
+<body>
+
+    <header>
+        <nav>
+            <a href="#">
+                <img src="images/xLogo.jpg" alt="logo">
+            </a>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">About Me</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
--- index.php
+++ index.php
@@ -1,19 +1,20 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Sign Up Form Template</title>
-	<link rel="stylesheet" type="text/css" href="">
-</head>
-<body>
-    <h2>Sign Up</h2>
-    <form action="includes/signup.inc.php" method="POST">
-        <input type="text" name="firstName" placeholder="Prénom">
-        <input type="text" name="lastName" placeholder="Nom de famille">
-        <input type="text" name="email" placeholder="E-mail">
-        <input type="text" name="username" placeholder="Identifiant">
-        <input type="text" name="password" placeholder="Mot de Passe">
-        <button type="submit" name="submit">Créer Un Compte</button>
-    </form>
-</body>
-</html>
+<?php
+    require "inc/_header.php";
+?>
+
+    <main>
+        <h2>Sign Up</h2>
+        <form action="inc/signup.inc.php" method="POST">
+            <input type="text" name="firstName" placeholder="Prénom">
+            <input type="text" name="lastName" placeholder="Nom de famille">
+            <input type="text" name="email" placeholder="E-mail">
+            <input type="text" name="username" placeholder="Identifiant">
+            <input type="text" name="password" placeholder="Mot de Passe">
+            <button type="submit" name="submit">Créer Un Compte</button>
+        </form>
+    </main>
+
+<?php
+    require "inc/_footer.php";
+?>
 
