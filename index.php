4ec2433b2bdffd8e47f772c25d92db6b53bdc120
Shannon Durkin-Bouiges <ShannonDurkinBouiges@gmail.com>
2020-09-21 14:54:32 +0200

    Created html sign up form

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sign Up Form Template</title>
+	<link rel="stylesheet" type="text/css" href="">
+</head>
+<body>
+    <h2>Sign Up</h2>
+    <form action="includes/signup.inc.php" method="POST">
+        <input type="text" name="firstName" placeholder="Prénom">
+        <input type="text" name="lastName" placeholder="Nom de famille">
+        <input type="text" name="email" placeholder="E-mail">
+        <input type="text" name="username" placeholder="Identifiant">
+        <input type="text" name="password" placeholder="Mot de Passe">
+        <button type="submit" name="submit">Créer Un Compte</button>
+    </form>
+</body>
+</html>
+
