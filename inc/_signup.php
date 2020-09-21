b80a4d720bca2e209ffaab226c166eb62389ed7f
Shannon Durkin-Bouiges <ShannonDurkinBouiges@gmail.com>
2020-09-21 22:00:14 +0200

    Updated statements to check for errors in signup form

--- inc/_signup.php
+++ inc/_signup.php
@@ -31,10 +31,37 @@ if (isset($_POST['signupButton'])) {
         if (!mysqli_stmt_prepare($stmt, $sql)) {
             header("Location: ../signup.php?error=sqlerror");
             exit();
-        }  elseif () {
-        
+        }  else {
+            mysqli_stmt_bind_param($stmt, "s", $username);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_store_result($stmt);
+            $resultCheck = mysqli_stmt_num_rows($stmt);
+            if ($resultCheck > 0) {
+                header("Location: ../signup.php?error=usertaken&email=".$email);
+                exit();
+            }  else {
+                $sql = "INSERT INTO users (username_Users, email_Users, password_Users) VALUES (?, ?, ?)";
+                $stmt = mysqli_stmt_init($conn);
+                if (!mysqli_stmt_prepare($stmt, $sql)) {
+                    header("Location: ../signup.php?error=sqlerror");
+                    exit();
+                } else {
+                    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+                    
+                    mysqli_stmt_bind_param($stmt, "sss", $username, $email, $hashedPassword);
+                    mysqli_stmt_execute($stmt);
+                    header("Location: ../signup.php?signup=success");
+                    exit();
+                }
+            }
         }
         
     }
-}
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
+    
+}  else {
+    header("Location: ../signup.php");
+    exit();
+   }
 
