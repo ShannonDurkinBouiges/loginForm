c0ec427e7655ef194011c2db89d062695bcf66b4
Shannon Durkin-Bouiges <ShannonDurkinBouiges@gmail.com>
2020-09-22 15:55:39 +0200

    Added error messages to signup form

--- signup.php
+++ signup.php
@@ -5,6 +5,25 @@
     <main>
         <section>
             <h1>Signup</h1>
+            <?php
+                if (isset($_GET['error'])) {
+                    if ($_GET['error'] == "emptyfieds") {
+                        echo '<p>Fill in all fields</p>';
+                    } elseif ($_GET['error'] == "invalidemailusername") {
+                        echo '<p>Invalid e-mail or username</p>';
+                    }  elseif ($_GET['error'] == "invalidemail") {
+                        echo '<p>Invalid e-mail</p>';
+                    }  elseif ($_GET['error'] == "invalidusername") {
+                        echo '<p>Invalid username</p>';
+                    }  elseif ($_GET['error'] == "usertaken") {
+                        echo '<p>Username already taken</p>';
+                    }  elseif ($_GET['error'] == "passwordcheck") {
+                        echo '<p>Passwords do not match</p>';
+                    }
+                }  elseif ($_GET['signup'] == "success") {
+                    echo '<p>Signup successful</p>';
+                }
+            ?>
             <form action="inc/_signup.php" method="POST">
                 <input type="text" name="signupUsername" placeholder="Usernamer">
                 <input type="text" name="signupEmail" placeholder="Email">
